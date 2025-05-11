82c26b4347dba8c8260339fae0800b69ec182fd3
Anthony Si <yatkasi@gmail.com>
2025-05-11 11:33:12 -0400

    use english model instead

--- static/scripts/voice.js
+++ static/scripts/voice.js
@@ -4,38 +4,37 @@ import { myId } from './signaling.js';
 async function start() {
   const { sharedAudioContext, sharedMediaStream } = await initializeAudio();
 
-  // Setup microphone
   let micNode = sharedAudioContext.createMediaStreamSource(sharedMediaStream);
-
-  // Load Vosklet module, model, and recognizer
   let module = await loadVosklet();
-  let model = await module.createModel("/static/model/vosk-model-small-cn-0.22.tar.gz", "Chinese", "vosk-model-small-cn-0.22");
-  let recognizer = await module.createRecognizer(model, sharedAudioContext.sampleRate);
+  // let model = await module.createModel("/static/model/vosk-model-small-cn-0.22.tar.gz", "Chinese", "vosk-model-small-cn-0.22");
+  let model = await module.createModel("/static/model/vosk-model-small-en-us-0.15.tar.gz", "English", "vosk-model-small-en-us-0.15");
+  const targetWords = [
+    "Eat", "Bump", "Hit", "Throw", "Woo"
+  ];
+  const recognizer = await module.createRecognizerWithGrm(model, 16000,
+    JSON.stringify(targetWords));
 
-  // Listen for result and partial result
   recognizer.addEventListener("result", ev => {
     const resultElement = document.getElementById(`full-result-${myId}`);
     if (resultElement) {
-      const resultData = JSON.parse(ev.detail); 
-      resultElement.textContent = `Full: ${resultData.text}`;
+      const resultData = JSON.parse(ev.detail); // Parse the JSON string
+      const recognizedText = resultData.text;
+
+      resultElement.textContent = `Full: ${recognizedText}`;
+      console.log(`Recognized command: ${recognizedText}`);
     }
   });
 
   recognizer.addEventListener("partialResult", ev => {
     const partialResultElement = document.getElementById(`partial-result-${myId}`);
     if (partialResultElement) {
-        const resultData = JSON.parse(ev.detail); 
-        partialResultElement.textContent = `Partial: ${resultData.partial}`;
+      const partialData = JSON.parse(ev.detail); // Parse the JSON string
+      partialResultElement.textContent = `Partial: ${partialData.partial}`;
     }
   });
 
-  // Create a transferer node to get audio data on the main thread
-  let transferer = await module.createTransferer(sharedAudioContext, 128 * 150);
-
-  // Recognize data on arrival
+  const transferer = await module.createTransferer(sharedAudioContext, 128 * 150);
   transferer.port.onmessage = ev => recognizer.acceptWaveform(ev.data);
-
-  // Connect transferer to microphone
   micNode.connect(transferer);
 }
 
